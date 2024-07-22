2f96bef101026f7757ee6cd24f74fb28ef6a7844
Sho8000 <yoshikichi8000@gmail.com>
2024-07-22 15:33:33 +0900

    All Link Added

---
+++ src/scriptFiles/service.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const navbar = document.getElementById("navbar");
+  const logo = document.getElementById("mainLogo");
+  const openTag = document.getElementById("opentagSB");
+  const closeTag = document.getElementById("closetagSB");
+  const sidebar = document.getElementById("sidebar");
+  const DIYSlide = document.getElementById("DIY_Video");
+  const DIYImg = document.getElementsByClassName("DIYimg")
+
+  window.addEventListener("scroll", () => {
+    const scroll = window.scrollY;
+    if (scroll >= 10) {
+      navbar.classList.add("smallNavBar");
+      logo.src = "resources/landing/ShrinkedLogo_Navbar.svg";
+    } else {
+      navbar.classList.remove("smallNavBar");
+      logo.src = "resources/landing/CarlaBeautyLogo.svg";
+    }
+  });
+
+  openTag.addEventListener("click", () => {
+    // console.log("clicked");
+    sidebar.style.display = "flex";
+  });
+  closeTag.addEventListener("click", () => {
+    // console.log("clicked");
+    sidebar.style.display = "none";
+  });
+
+});
