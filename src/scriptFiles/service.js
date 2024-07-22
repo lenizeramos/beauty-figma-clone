3732cff440f17ec9b1df0de46bf7112fbfb62983
ivanmatey <ivanmatey5@gmail.com>
2024-07-22 10:24:30 -0600

    fixed the navbar on services page

--- src/scriptFiles/service.js
+++ src/scriptFiles/service.js
@@ -5,16 +5,16 @@ document.addEventListener("DOMContentLoaded", () => {
   const closeTag = document.getElementById("closetagSB");
   const sidebar = document.getElementById("sidebar");
   const DIYSlide = document.getElementById("DIY_Video");
-  const DIYImg = document.getElementsByClassName("DIYimg")
+  const DIYImg = document.getElementsByClassName("DIYimg");
 
   window.addEventListener("scroll", () => {
     const scroll = window.scrollY;
     if (scroll >= 10) {
       navbar.classList.add("smallNavBar");
-      logo.src = "resources/landing/ShrinkedLogo_Navbar.svg";
+      logo.src = "../resources/landing/ShrinkedLogo_Navbar.svg";
     } else {
       navbar.classList.remove("smallNavBar");
-      logo.src = "resources/landing/CarlaBeautyLogo.svg";
+      logo.src = "../resources/landing/CarlaBeautyLogo.svg";
     }
   });
 
@@ -26,5 +26,4 @@ document.addEventListener("DOMContentLoaded", () => {
     // console.log("clicked");
     sidebar.style.display = "none";
   });
-
 });
